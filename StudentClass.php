fe8d7b5ed517f2b158515ef391825b92d64baced
jvaj72 <jessicadaunay@gmail.com>
2016-10-05 10:16:19 +0200

    cours_php_objet

---
+++ StudentClass.php
@@ -0,0 +1,22 @@
+<?php
+
+Class Student{
+private $nom;
+private $prenom;
+public $age
+
+public __construc ($nom,$prenom){
+	$this-> nom = $nom
+	$this-> prenom = $prenom
+	}
+	
+public getNom(){
+	return $this -> nom;
+}
+
+public getPrenom(){
+	return $this -> Prenom;
+	}
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+$Student1 = new Student ("Gauchard","Yoann");
+$Student2 = new Student ("Dupond","Marie");
+$Student3 = new Student ("Debray","Marie");
+
+	echo $Student1 -> getNom();
+	echo $Student1 -> age;
+
+?>
